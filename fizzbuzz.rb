beeafdc50ba48dfd404beb914c90c83678594891
Jacob Barry <the.jacobbarry@gmail.com>
2018-11-27 21:00:31 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(integer)
+  
+ if integer % 3 == 0 && integer % 5 == 0
+     "FizzBuzz"
+  elsif integer % 3 == 0
+     "Fizz"
+  elsif integer % 5 == 0
+     "Buzz"
+  
+  else 
+     nil
+  end
+end
