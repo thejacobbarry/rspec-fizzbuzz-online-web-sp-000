1ad27434c3839c64ad5795559e51da13e9793a07
Jacob Barry <the.jacobbarry@gmail.com>
2018-11-27 20:11:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(Integer)
+  Integer = gets
+  if Integer % 3 == 0
+    puts "Fizz"
+  elsif Integer % 5 == 0
+    puts "Buzz"
+  elsif Integer % 3 == 0 && Integer % 5 == 0
+    puts "FizzBuzz"
+    else puts "nill"
+  end
+  
